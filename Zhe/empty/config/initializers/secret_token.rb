666e000faa99d606d6e911fc215c03debcfed6bd
Ubuntu <ubuntu@ip-10-255-10-185.us-west-2.compute.internal>
2013-11-05 22:53:46 +0000

    copy empty to Zhe's folder

---
+++ Zhe/empty/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Empty::Application.config.secret_token = '1fd445857f4b8347c9ea35458337ccdaf06b02ac3c9e9fc602316e1a1a7db0fd98e9716111136ba29afdaeace54602463e93d14d05bb7343d6f6ea1140239dd9'
